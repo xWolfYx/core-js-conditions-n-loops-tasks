e1897615f3bfdf3b84da01cdc4181fb6976c383d
xWolfYx <david.skliarov@gmail.com>
2024-12-25 16:08:10 +0400

    feat: resolve function isPositive

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,11 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number < 0) {
+    return false;
+  }
+  return true;
 }
 
 /**
